5209b0b4563dda9ad24ce9492b39aeba96e15686
Kyrillosmaher7 <kirolos7maher@gmail.com>
2026-09-21 21:00:43 +0300

    Refix CORS

--- Dalleni.API/Program.cs
+++ Dalleni.API/Program.cs
@@ -97,30 +97,30 @@ builder.Services.AddSwaggerGen(c =>
 #endregion
 
 #region ------------------------ AllowCORS ------------------------------
-// var cors = "_DefaultCors";
-// builder.Services.AddCors(options =>
-// {
-//     options.AddPolicy(name: cors, policy =>
-//     {
-//         policy.AllowAnyHeader();
-//         policy.AllowAnyMethod();
-//         policy.AllowAnyOrigin();
-//     });
-// });
-
 var cors = "_DefaultCors";
-
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy(cors, policy =>
+    options.AddPolicy(name: cors, policy =>
     {
-        policy
-            .WithOrigins("http://localhost:5500")
-            .AllowAnyHeader()
-            .AllowAnyMethod()
-            .AllowCredentials();
+        policy.AllowAnyHeader();
+        policy.AllowAnyMethod();
+        policy.AllowAnyOrigin();
     });
 });
+
+// var cors = "_DefaultCors";
+
+// builder.Services.AddCors(options =>
+// {
+//     options.AddPolicy(cors, policy =>
+//     {
+//         policy
+//             .WithOrigins("http://localhost:5500")
+//             .AllowAnyHeader()
+//             .AllowAnyMethod()
+//             .AllowCredentials();
+//     });
+// });
 #endregion
 
 #region --------------------- configure JSON enum serialization globally --------------
